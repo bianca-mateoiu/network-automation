49ffc3ad4ce72933d0f8fc36798d3fd94f879ed7
bianca-mateoiu <52234359+bianca-mateoiu@users.noreply.github.com>
2020-03-17 11:44:05 +0000

    Update test.py

--- Netbox Initial Setup/test.py
+++ Netbox Initial Setup/test.py
@@ -6,5 +6,9 @@ from Netbox_REST_APIs import *
 
 #create_ip_addresses('core_links')
 #create_ip_addresses('pe_cpe')
-create_ip_addresses('LAN')
-create_ip_addresses('management')
+
+
+
+create_interface_templates('vMX')
+
+create_interface_templates('Nexus')
